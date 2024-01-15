0d560c59c58d2eb6ca410c4f41052ddffd090f98
Kusal Rajapaksha <kusal@4axissolutions.com>
2024-01-15 22:48:34 +0530

    Access level changes

--- Sources/SwiftUIToast/Toast/Toast.swift
+++ Sources/SwiftUIToast/Toast/Toast.swift
@@ -21,4 +21,18 @@ public struct Toast: Equatable{
     var maskedCorners: [CornerMasks]? = nil
     var horizontalPadding: CGFloat = 16
     var verticalPadding: CGFloat = 6
+    
+    public init(style: ToastStyle, message: String, duration: Double, width: Double, cornerRadius: CGFloat, fontName: String, fontSize: CGFloat, numberOfLines: Int, maskedCorners: [CornerMasks]? = nil, horizontalPadding: CGFloat, verticalPadding: CGFloat) {
+        self.style = style
+        self.message = message
+        self.duration = duration
+        self.width = width
+        self.cornerRadius = cornerRadius
+        self.fontName = fontName
+        self.fontSize = fontSize
+        self.numberOfLines = numberOfLines
+        self.maskedCorners = maskedCorners
+        self.horizontalPadding = horizontalPadding
+        self.verticalPadding = verticalPadding
+    }
 }
